a82070cbc61f8d987ef2f035600b1a9b05ebe405
Fesantt <felipe99611130@gmail.com>
2024-07-09 21:21:50 -0500

    os update

---
+++ lib/pages/os/tabs/detalhes_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DetalhesTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Detalhes da OS'),
+    );
+  }
+}
---
+++ lib/pages/os/tabs/produtos_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ProdutosTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Conteúdo da Tab 1'),
+    );
+  }
+}
